9829ce72d4610226b29a68a85d79154291bebabf
Matt Bird <bird3@umbc.edu>
2016-10-01 16:49:34 -0400

    Initial Android App commit

---
+++ Android/StudiBot/app/src/main/java/hackumchfall16/studibot/STUDIBOT.java
@@ -0,0 +1,13 @@
+package hackumchfall16.studibot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class STUDIBOT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_studibot);
+    }
+}
